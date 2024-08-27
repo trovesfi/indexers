fcdc3e2435b5ae61870bf28e60b4bf0cb69c9cf0
akiraonstarknet <zkdice@proton.me>
2024-08-27 18:13:41 +0530

    update docker to run multiple apibara indexers [2]

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+echo "Running dep-withdraw and harvests"
 nohup yarn run start-dep-withdraw > dep-withdraw.log &
 nohup yarn run start-harvests > harvests.log &
+echo "Running dep-withdraw and harvests"
+tail -100f dep-withdraw.log
