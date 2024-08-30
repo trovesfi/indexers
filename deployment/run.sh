358f59f36611c7bf9741cf08c73a9a41431b75a2
akiraonstarknet <zkdice@proton.me>
2024-08-30 18:32:11 +0530

    Dockerfile [12]

--- deployment/run.sh
+++ deployment/run.sh
@@ -5,7 +5,14 @@ if ! command -v grpcurl >/dev/null 2>&1; then
 fi
 
 echo "Running indexers"
-/root/.local/share/apibara/bin/apibara run --allow-env=.env src/strkfarm/deposits-withdraws.ts --sink-id=130 --status-server-address=0.0.0.0:4130
+nohup /root/.local/share/apibara/bin/apibara run --allow-env=.env src/strkfarm/deposits-withdraws.ts --sink-id=130 --status-server-address=0.0.0.0:4130 > dep-withdraw.log &
 nohup /root/.local/share/apibara/bin/apibara run --allow-env=.env src/strkfarm/harvests.ts --sink-id=140 --status-server-address=0.0.0.0:4140 > harvests.log &
+
+echo "Sleeping 10s"
+sleep 10
+cat dep-withdraw.log
+echo "==================================================="
+cat harvests.log
+
 echo "Starting server"
 node index.js
