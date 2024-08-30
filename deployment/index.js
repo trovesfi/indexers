b9734674584d198e7e3869b37e5c3dcc0b20a879
akiraonstarknet <zkdice@proton.me>
2024-08-30 17:47:09 +0530

    Dockerfile [9]

--- deployment/index.js
+++ deployment/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { spawn } = require('child_process');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Function to execute grpcurl command and get the status
 function getGrpcStatus(grpcPort) {
