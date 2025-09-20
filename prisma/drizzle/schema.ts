8854b04293fc2bd6bfd20227915f9053991fb30a
akiraonstarknet <zkdice@proton.me>
2025-09-20 17:35:12 +0530

    prisma folder reorg

---
+++ prisma/drizzle/schema.ts
@@ -0,0 +1,118 @@
+import { sql } from 'drizzle-orm'
+import { bigint, decimal, doublePrecision, integer, pgTable, text, uniqueIndex } from 'drizzle-orm/pg-core'
+
+export const investment_flows = pgTable('investment_flows', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	block_number: integer('block_number').notNull(),
+	tx_index: integer('tx_index').notNull(),
+	event_index: integer('event_index').notNull(),
+	tx_hash: text('tx_hash').notNull(),
+	sender: text('sender').notNull(),
+	receiver: text('receiver').notNull(),
+	owner: text('owner').notNull(),
+	amount: text('amount').notNull(),
+	shares: text('shares').notNull(),
+	asset: text('asset').notNull(),
+	contract: text('contract').notNull(),
+	epoch: integer('epoch').notNull(),
+	request_id: integer('request_id').notNull(),
+	type: text('type').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	cursor: bigint('_cursor', { mode: 'bigint' })
+}, (investment_flows) => ({
+	'event_id': uniqueIndex('event_id')
+		.on(investment_flows.block_number, investment_flows.tx_index, investment_flows.event_index)
+}));
+
+export const harvests = pgTable('harvests', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	block_number: integer('block_number').notNull(),
+	tx_index: integer('tx_index').notNull(),
+	event_index: integer('event_index').notNull(),
+	tx_hash: text('tx_hash').notNull(),
+	user: text('user').notNull(),
+	contract: text('contract').notNull(),
+	amount: text('amount').notNull(),
+	price: doublePrecision('price').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	cursor: bigint('_cursor', { mode: 'bigint' })
+}, (harvests) => ({
+	'event_id': uniqueIndex('event_id')
+		.on(harvests.block_number, harvests.tx_index, harvests.event_index)
+}));
+
+export const position_fees_collected = pgTable('position_fees_collected', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	block_number: integer('block_number').notNull(),
+	tx_index: integer('tx_index').notNull(),
+	event_index: integer('event_index').notNull(),
+	tx_hash: text('tx_hash').notNull(),
+	token0: text('token0').notNull(),
+	token1: text('token1').notNull(),
+	amount0: text('amount0').notNull(),
+	amount1: text('amount1').notNull(),
+	vault_address: text('vault_address').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	cursor: bigint('_cursor', { mode: 'bigint' })
+}, (position_fees_collected) => ({
+	'event_id': uniqueIndex('event_id')
+		.on(position_fees_collected.block_number, position_fees_collected.tx_index, position_fees_collected.event_index)
+}));
+
+export const position_updated = pgTable('position_updated', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	block_number: integer('block_number').notNull(),
+	tx_index: integer('tx_index').notNull(),
+	event_index: integer('event_index').notNull(),
+	tx_hash: text('tx_hash').notNull(),
+	locker: text('locker').notNull(),
+	token0: text('token0').notNull(),
+	token1: text('token1').notNull(),
+	fee: text('fee').notNull(),
+	tick_spacing: text('tick_spacing').notNull(),
+	extension: text('extension').notNull(),
+	salt: text('salt').notNull(),
+	lower_bound: text('lower_bound').notNull(),
+	upper_bound: text('upper_bound').notNull(),
+	liquidity_delta: text('liquidity_delta').notNull(),
+	amount0: text('amount0').notNull(),
+	amount1: text('amount1').notNull(),
+	vault_address: text('vault_address').notNull(),
+	user_address: text('user_address').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	cursor: bigint('_cursor', { mode: 'bigint' })
+}, (position_updated) => ({
+	'event_id': uniqueIndex('event_id')
+		.on(position_updated.block_number, position_updated.tx_index, position_updated.event_index)
+}));
+
+export const raw_price_events = pgTable('raw_price_events', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	block_number: integer('block_number').notNull(),
+	tx_index: integer('tx_index').notNull(),
+	event_index: integer('event_index').notNull(),
+	tx_hash: text('tx_hash').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	source: text('source').notNull(),
+	publisher: text('publisher').notNull(),
+	price: decimal('price', { precision: 65, scale: 30 }).notNull(),
+	pair_id: text('pair_id').notNull(),
+	volume: decimal('volume', { precision: 65, scale: 30 }).notNull()
+}, (raw_price_events) => ({
+	'event_id': uniqueIndex('event_id')
+		.on(raw_price_events.block_number, raw_price_events.tx_index, raw_price_events.event_index)
+}));
+
+export const prices = pgTable('prices', {
+	id: text('id').notNull().primaryKey().default(sql`gen_random_uuid()`),
+	asset: text('asset').notNull(),
+	price: doublePrecision('price').notNull(),
+	price_sum: doublePrecision('price_sum').notNull(),
+	sources_count: integer('sources_count').notNull(),
+	timestamp: integer('timestamp').notNull(),
+	block_number: integer('block_number').notNull(),
+	cursor: bigint('_cursor', { mode: 'bigint' })
+}, (prices) => ({
+	'price_id': uniqueIndex('price_id')
+		.on(prices.asset, prices.timestamp)
+}));
